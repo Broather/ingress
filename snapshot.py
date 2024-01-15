47b447f219dfbd1b9f53077ed6cd9fa843600de4
Markuss Grinhāgens <markussgr@gmail.com>
2024-01-15 23:52:29 +0200

    fixed a major bug in is_loop, sets are a new technology for me, gimme a break. Added -l option for rendering only links

--- snapshot.py
+++ snapshot.py
@@ -20,13 +20,13 @@ class Link:
         else:
             return False
 
-    def intersection(self, other: object) -> Portal|None:
+    def intersection(self, other: object) -> set:
         if isinstance(other, Link) and self.is_touching(other):
             return self.portals & other.portals 
 
     def is_loop(self, one, other) -> bool:
         if isinstance(one, Link) and isinstance(other, Link):
-            return self.is_touching(one) and self.is_touching(other) and one.is_touching(other) and one.intersection(other) not in self.portals
+            return self.is_touching(one) and self.is_touching(other) and one.is_touching(other) and one.intersection(other).isdisjoint(self.portals)
         else:
             return False
             
@@ -38,7 +38,7 @@ class Field:
         return f"{self.portals}"
 
 def help():
-    print("Syntax: python snapshot.py [-h] [-p comma_separated_list[<PV|VP|..>]] path/to/plan.json step_number")
+    print("Syntax: python snapshot.py [-hl] [-p comma_separated_list[<PV|VP|..>]] path/to/plan.json step_number")
     print("""simulates going through the plan's steps and creates snapshot.json as the progress when stopped at step_number 
     
     Arguments:
@@ -46,6 +46,7 @@ def help():
         step_number:int numbered step to take the snapshot at
     Options:
         h: calls this help function
+        h: renders only links
         p: defines which portal groups to use in making fields (only way I could think of to get portal data here)
     """)
 
@@ -54,9 +55,13 @@ def main(opts: list[tuple[str, str]], args):
     path = args[0]
     step_to_stop_at = int(args[1])
 
+    # defaults
+    only_links = False
     for o, a in opts:
         if o == "-h":
             help()
+        elif o == "-l":
+            only_links = True
         elif o == "-p":
             for portal_group in a.split(","):
                 with open(Ingress.portal_group_map[portal_group.strip()], "r", encoding='utf-8') as f:
@@ -68,7 +73,7 @@ def main(opts: list[tuple[str, str]], args):
             input = json.load(f)
         steps: dict = input[0]["Steps"]
     except FileNotFoundError:
-        print(f"{path} no found")
+        print(f"{path} not found")
         return
 
     relevant_steps = list(itertools.islice(steps, step_to_stop_at))
@@ -80,18 +85,18 @@ def main(opts: list[tuple[str, str]], args):
     for key in relevant_steps[:-1]:
         for other in steps[key]["links"]:
             # output.append(Link(key, other))
-            output = my_append(Link(key, other, green), output)
+            output = my_append(Link(key, other, green), output, only_links)
 
     # last step is colored blue
     last_step = relevant_steps[-1]
     for other in steps[last_step]["links"]:
         # output.append(Link(key, other))
-        output = my_append(Link(last_step, other, blue), output)
+        output = my_append(Link(last_step, other, blue), output, only_links)
     
     output = list(map(render, output))
     Ingress.output_to_json(output, "./snapshot.json")
 
-def my_append(link: Link, output: list) -> list:
+def my_append(link: Link, output: list, only_links: bool) -> list:
     """
     checks to see if field or fields can be made from link and the current state of things (output) 
     """
@@ -99,9 +104,8 @@ def my_append(link: Link, output: list) -> list:
     is_touching_map = list(map(link.is_touching, output))
     amount_of_links_touching = sum(is_touching_map)
     print(f"There are {amount_of_links_touching} links touching {link}")
-    if amount_of_links_touching < 2:
-        output.append(link)
-    else:
+    output.append(link)
+    if not only_links:
         # check if any combination of link and 2 links that are touching create a closed loop
         touching_links = [l for l, is_touching in zip(output, is_touching_map) if is_touching]
         for one, other_one in itertools.combinations(touching_links, 2):
@@ -111,7 +115,6 @@ def my_append(link: Link, output: list) -> list:
                 output.append(field)
 
     return output
-
     
     return output
 def render(object: Link|Field) -> dict:
@@ -132,5 +135,5 @@ def render(object: Link|Field) -> dict:
     return data
 
 if __name__ == "__main__":
-    opts, args = getopt.getopt(sys.argv[1:], "hp:", [])
+    opts, args = getopt.getopt(sys.argv[1:], "hlp:", [])
     main(opts, args)
