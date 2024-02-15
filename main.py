a961d870a4d9380a78414d24de64f92c3883fde1
Markuss Grinhāgens <markussgr@gmail.com>
2024-02-15 22:13:36 +0200

    why was 6 afraid of 7? Because I made some improvements to 7 8 9. Yea idk, it's a commit message, noone cares

--- main.py
+++ main.py
@@ -497,15 +497,15 @@ class Ingress:
                     field_level))
             elif digit == "7":
                 output.append(Field.from_route(
-                    tuple(map(portal_grid.get, ((0,0),(0,4),(1,4),(2,3),(3,3),(3,4),(4,4),(4,1),(3,1),(3,2),(2,2),(1,3),(1,1)))),
+                    tuple(map(portal_grid.get, ((0,0),(0,4),(1,4),(2,2),(2,3),(3,3),(3,2),(4,2),(4,1),(3,1),(3,0),(2,0),(2,1),(1,3),(1,1)))),
                     field_level))
             elif digit == "8":
                 output.append(Field.from_route(
-                    tuple(map(portal_grid.get, ((1,0),(0,1),(0,2),(1,3),(1,1),(0,2),(0,3),(1,4),(2,3),(3,2),(2,1),(2,3),(3,4),(4,3),(4,1),(3,0),(2,1)))),
+                    tuple(map(portal_grid.get, ((1,0),(0,1),(1,2),(2,1),(0,1),(0,2),(1,2),(1,3),(2,4),(3,4),(4,3),(4,2),(3,1),(2,2),(2,3),(3,3),(4,2),(4,1),(3,0),(2,1),))),
                     field_level))
             elif digit == "9":
                 output.append(Field.from_route(
-                    tuple(map(portal_grid.get, ((1,0),(0,1),(0,2),(1,3),(1,1),(0,2),(0,3),(1,4),(3,4),(4,3),(4,0),(2,0),(3,1),(4,1),(2,3),(2,1)))),
+                    tuple(map(portal_grid.get, ((1,0),(0,1),(0,3),(1,4),(3,4),(4,3),(4,1),(3,0),(2,0),(4,2),(3,3),(1,3),(1,1),(0,2),(1,3),(2,1),))),
                     field_level))
         # TODO: in the future have from_route return a tuple of fields that are all triangles (super dynamic styles) but I'll keep this for now because it looks good
         return tuple(output)
